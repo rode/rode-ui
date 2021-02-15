cac95f21c0535a8497b5eda334aa4d264a11176b
Alex Ashley <alexa@liatrio.com>
2021-02-15 13:19:08 -0600

    Add jest-dom helpers

--- test/pages/index.spec.js
+++ test/pages/index.spec.js
@@ -8,6 +8,6 @@ describe("index", () => {
     render(<Home />);
     const expected =
       "Instantly deploy your Next.js site to a public URL with Vercel.";
-    expect(screen.queryByText(expected)).not.toBeNull();
+    expect(screen.queryByText(expected)).toBeInTheDocument();
   });
 });
