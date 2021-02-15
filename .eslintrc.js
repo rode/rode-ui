b583e723c844fc6183420a0855fee67e2fceba82
Alex Ashley <alexa@liatrio.com>
2021-02-15 11:51:02 -0600

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = { // eslint-disable-line no-undef
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+};
