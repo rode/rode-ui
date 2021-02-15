ef44856d1ecebb717a5b455f4dc34534d4328e45
Alex Ashley <alexa@liatrio.com>
2021-02-15 12:15:33 -0600

    wip jest tests

---
+++ test/pages/index.spec.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react'
+import Home from '../../pages/index';
+
+describe('index', () => {
+    it('should pass', () => {
+        render(<Home />)
+        const expected = "Instantly deploy your Next.js site to a public URL with Vercel.";
+        expect(screen.getByText(expected)).toBeDefined();
+    });
+});
