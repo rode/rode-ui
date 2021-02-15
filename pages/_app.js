3b4e53a1ec83cfb84770a861b3ce21276693633a
Alex Ashley <alexa@liatrio.com>
2021-02-15 13:28:38 -0600

    Initial setup (#2)

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import React from "react";
+import PropTypes from "prop-types";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+MyApp.propTypes = {
+  Component: PropTypes.node,
+  pageProps: PropTypes.object,
+};
+
+export default MyApp;
---
+++ test/pages/index.spec.js
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+
+import Home from "../../pages/index";
+
+describe("index", () => {
+  it("should pass", () => {
+    render(<Home />);
+    const expected =
+      "Instantly deploy your Next.js site to a public URL with Vercel.";
+    expect(screen.queryByText(expected)).toBeInTheDocument();
+  });
+});
