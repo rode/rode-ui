205f322fee6d906aff63015f865d7651e2f0a89b
Jasmine Francois <jfrancois@leantechniques.com>
2021-02-18 16:52:02 -0600

    Fix weird styling on homepage after dark theme changes

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import React from "react";
+import styles from "styles/modules/Resources.module.scss";
 import ResourceSearchBar from "components/resources/ResourceSearchBar";
 
 const Home = () => {
-  return <ResourceSearchBar />;
+  return (
+    <div className={styles.containerNoResults}>
+      <ResourceSearchBar />
+    </div>
+  );
 };
 
 export default Home;
