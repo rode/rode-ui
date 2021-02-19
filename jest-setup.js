83fcbd66e184ee1500f53bad5f91ec3e5e30058f
Alex Ashley <alexa@liatrio.com>
2021-02-19 11:12:00 -0600

    Add api tests

--- jest-setup.js
+++ jest-setup.js
@@ -1,5 +1,9 @@
 import "@testing-library/jest-dom";
+import "jest-chain";
 import Chance from "chance";
 
 // eslint-disable-next-line no-unused-vars
 const chance = new Chance();
+
+// suppress logs
+console.error = jest.fn();
--- pages/api/resources.js
+++ pages/api/resources.js
@@ -1,35 +1,38 @@
-import {StatusCodes, ReasonPhrases} from 'http-status-codes';
-import fetch from 'node-fetch'
+import { StatusCodes, ReasonPhrases } from "http-status-codes";
+import fetch from "node-fetch";
 
-const getRodeUrl = () => process.env.RODE_URL || 'http://localhost:50052'
+const getRodeUrl = () => process.env.RODE_URL || "http://localhost:50052";
 
 export default async (req, res) => {
-    if (req.method !== 'GET') {
-        return res
-            .status(StatusCodes.METHOD_NOT_ALLOWED)
-            .json({error: ReasonPhrases.METHOD_NOT_ALLOWED});
+  if (req.method !== "GET") {
+    return res
+      .status(StatusCodes.METHOD_NOT_ALLOWED)
+      .json({ error: ReasonPhrases.METHOD_NOT_ALLOWED });
+  }
+
+  const rodeUrl = getRodeUrl();
+
+  try {
+    const response = await fetch(`${rodeUrl}/v1alpha1/resources`);
+    if (!response.ok) {
+      console.error(`Unsuccessful response from Rode: ${response.status}`);
+      return res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ error: ReasonPhrases.INTERNAL_SERVER_ERROR });
     }
 
-    const rodeUrl = getRodeUrl();
+    const listResourcesResponse = await response.json();
+    const resources = listResourcesResponse.resources.map(({ name, uri }) => ({
+      name,
+      uri,
+    }));
 
-    try {
-        const response = await fetch(`${rodeUrl}/v1alpha1/resources`);
-        if (!response.ok) {
-            console.error(`Unsuccessful response from Rode: ${response.status}`)
-            return res
-                .status(StatusCodes.INTERNAL_SERVER_ERROR)
-                .json({error: ReasonPhrases.INTERNAL_SERVER_ERROR});
-        }
+    res.status(StatusCodes.OK).json(resources);
+  } catch (error) {
+    console.error("Error listing resources", error);
 
-        const listResourcesResponse = await response.json();
-        const resources = listResourcesResponse.resources.map(({name, uri}) => ({name, uri}))
-
-        res.status(StatusCodes.OK).json(resources);
-    } catch (error) {
-        console.error('Error listing resources', error)
-
-        res
-            .status(StatusCodes.INTERNAL_SERVER_ERROR)
-            .json({error: ReasonPhrases.INTERNAL_SERVER_ERROR});
-    }
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: ReasonPhrases.INTERNAL_SERVER_ERROR });
+  }
 };
