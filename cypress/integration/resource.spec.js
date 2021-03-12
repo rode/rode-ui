910038fbaac77c778fbfeea082c3677148810478
Jasmine Francois <jfrancois@leantechniques.com>
2021-03-12 15:10:18 -0600

    Use environment variables for api and base urls

--- cypress/integration/resource.spec.js
+++ cypress/integration/resource.spec.js
@@ -1,30 +1,28 @@
 import { mockResource, mockResourceOccurrences } from "../mock-responses/resource-responses";
 
-const RODE_URL = "http://localhost:50054/v1alpha1";
-
 context('Resources', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000')
+    cy.visit('/')
     cy.get('a[href="/resources"]').click();
     cy.url().should('match', /\/resources$/);
   });
 
   it("should search for a resource that does not exist", () => {
-    cy.intercept(`${RODE_URL}/resources`, {});
+    cy.intercept(`${Cypress.env('api_url')}/resources`, {});
     cy.get('#resourceSearch').focus().type('not a match');
     cy.get('button[aria-label="Search"]').click();
     cy.contains('No resources found');
   });
 
   it("should search for a resource that does exist", () => {
-    cy.intercept(`${RODE_URL}/resources`, mockResource);
+    cy.intercept(`${Cypress.env('api_url')}/resources`, mockResource);
     cy.get('#resourceSearch').focus().type('nginx');
     cy.get('button[aria-label="Search"]').click();
     cy.contains('View Details');
   });
 
   it("should show resource details when a resource is selected", () => {
-    cy.intercept(`${RODE_URL}/resources`, mockResourceOccurrences);
+    cy.intercept(`${Cypress.env('api_url')}/resources`, mockResourceOccurrences);
 
     cy.get('#resourceSearch').focus().type('nginx');
     cy.get('button[aria-label="Search"]').click();
