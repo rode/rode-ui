329abe5b6ecdc4ecbf7233ab93d76145f16b3127
Jasmine Francois <jfrancois@leantechniques.com>
2021-03-12 14:23:33 -0600

    Clicking through different occurrence types

--- cypress/integration/resource.spec.js
+++ cypress/integration/resource.spec.js
@@ -1,4 +1,4 @@
-import { mockResource, mockResourceOccurrences } from "../mock-responses/resource-responses";
+import { mockResource, mockResourceOccurrences } from "cypress/mock-responses/resource-responses";
 
 const RODE_URL = "http://localhost:50054/v1alpha1";
 
@@ -25,8 +25,20 @@ context('Resources', () => {
 
   it("should show resource details when a resource is selected", () => {
     cy.intercept(`${RODE_URL}/resources`, mockResourceOccurrences);
+
     cy.get('#resourceSearch').focus().type('nginx');
     cy.get('button[aria-label="Search"]').click();
     cy.contains('View Details').click();
+
+    cy.contains('Produced 1 Artifact').click();
+    cy.get('button[aria-label="Show JSON"]').click();
+    cy.get('pre').should('be.visible');
+    cy.get('code').should('be.visible');
+
+    cy.contains('Vulnerability Scan').click();
+    cy.contains('Package: apt').should('be.visible');
+
+    cy.contains('Deployment to CUSTOM').click();
+    cy.contains('Deployed to CUSTOM').should('be.visible');
   });
 });
