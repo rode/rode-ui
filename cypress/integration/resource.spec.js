7c33228046db1f78ac63e1b7f0bee4d51a569e88
Jasmine Francois <jfrancois@leantechniques.com>
2021-03-15 09:34:11 -0500

    Linting

--- cypress/integration/resource.spec.js
+++ cypress/integration/resource.spec.js
@@ -1,44 +1,53 @@
-import { mockResource, mockResourceOccurrences } from "../mock-responses/resource-responses";
+import {
+  mockResource,
+  mockResourceOccurrences,
+} from "../mock-responses/resource-responses";
 
-context('Resources', () => {
+context("Resources", () => {
   beforeEach(() => {
-    cy.visit('/')
+    cy.visit("/");
     cy.get('a[href="/resources"]').click();
-    cy.url().should('match', /\/resources$/);
+    cy.url().should("match", /\/resources$/);
   });
 
   it("should search for a resource that does not exist", () => {
-    cy.intercept(`${Cypress.env('api_url')}/resources`, {});
-    cy.get('#resourceSearch').focus().type('not a match');
+    cy.intercept(`${Cypress.env("api_url")}/resources`, {});
+    cy.get("#resourceSearch").focus().type("not a match");
     cy.get('button[aria-label="Search"]').click();
-    cy.contains('No resources found');
+    cy.contains("No resources found");
   });
 
   it("should search for a resource that does exist", () => {
-    cy.intercept(`${Cypress.env('api_url')}/resources`, mockResource);
-    cy.get('#resourceSearch').focus().type('nginx');
+    cy.intercept(`${Cypress.env("api_url")}/resources`, mockResource);
+    cy.get("#resourceSearch").focus().type("nginx");
     cy.get('button[aria-label="Search"]').click();
-    cy.contains('View Details');
+    cy.contains("View Details");
   });
 
   it("should show resource details when a resource is selected", () => {
-    cy.intercept(`${Cypress.env('api_url')}/resources`, mockResourceOccurrences);
+    cy.intercept(
+      `${Cypress.env("api_url")}/resources`,
+      mockResourceOccurrences
+    );
 
-    cy.get('#resourceSearch').focus().type('nginx');
+    cy.get("#resourceSearch").focus().type("nginx");
     cy.get('button[aria-label="Search"]').click();
-    cy.contains('View Details').click();
+    cy.contains("View Details").click();
 
-    cy.get('button[class^="Occurrences_previewContainer"]').should('have.length', 5);
+    cy.get('button[class^="Occurrences_previewContainer"]').should(
+      "have.length",
+      5
+    );
 
-    cy.contains('Produced 1 Artifact').click();
+    cy.contains("Produced 1 Artifact").click();
     cy.get('button[aria-label="Show JSON"]').click();
-    cy.get('pre').should('be.visible');
-    cy.get('code').should('be.visible');
+    cy.get("pre").should("be.visible");
+    cy.get("code").should("be.visible");
 
-    cy.contains('Vulnerability Scan').click();
-    cy.contains('Package: apt').should('be.visible');
+    cy.contains("Vulnerability Scan").click();
+    cy.contains("Package: apt").should("be.visible");
 
-    cy.contains('Deployment to CUSTOM').click();
-    cy.contains('Deployed to CUSTOM').should('be.visible');
+    cy.contains("Deployment to CUSTOM").click();
+    cy.contains("Deployed to CUSTOM").should("be.visible");
   });
 });
