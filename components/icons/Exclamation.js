e449a8a1b752e779b13311a0084e3c4cb993687b
Jasmine Francois <jfrancois@leantechniques.com>
2021-03-22 16:36:32 -0500

    Add two new icons for pass/fail usage

---
+++ components/icons/Exclamation.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright 2021 The Rode Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from "react";
+
+const Exclamation = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+      <title>Exclamation</title>
+      <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+    </svg>
+  );
+};
+
+export default Exclamation;
--- components/policies/PolicyValidationResult.js
+++ components/policies/PolicyValidationResult.js
@@ -1,32 +1,38 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import styles from "styles/modules/Policy.module.scss";
+import { ICON_NAMES } from "utils/icon-utils";
+import Icon from "components/Icon";
 
 // TODO: improve the styles, add some icons and colors
 
-const PolicyValidationResult = ({validation}) => {
-
+const PolicyValidationResult = ({ validation }) => {
   if (!validation) {
     return null;
   }
 
   return (
     <div className={styles.validationResults}>
-      {
-        validation.isValid ?
+      {validation.isValid ? (
+        <>
+          <Icon name={ICON_NAMES.BADGE_CHECK} />
           <p>Policy is valid</p>
-          :
-          <>
+        </>
+      ) : (
+        <>
+          <Icon name={ICON_NAMES.EXCLAMATION} />
           <p>Policy is invalid</p>
-            <pre><code>{validation.errors}</code></pre>
-          </>
-      }
+          <pre>
+            <code>{validation.errors}</code>
+          </pre>
+        </>
+      )}
     </div>
-  )
+  );
 };
 
 PolicyValidationResult.propTypes = {
-  validation: PropTypes.object
-}
+  validation: PropTypes.object,
+};
 
 export default PolicyValidationResult;
