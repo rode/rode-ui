8074b95e2a9c227c09f006a3d48812aa6a2ca5d4
Jasmine Francois <jfrancois@leantechniques.com>
2021-04-08 15:33:05 -0500

    Toggle button will show/hide nav

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -14,54 +14,140 @@
  * limitations under the License.
  */
 
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import styles from "styles/modules/Header.module.scss";
 import RodeLogo from "./RodeLogo";
 import ThemeToggle from "./ThemeToggle";
 import { useTheme } from "providers/theme";
 import { useRouter } from "next/router";
+import Button from "../Button";
+import Icon from "../Icon";
+import { ICON_NAMES } from "../../utils/icon-utils";
 
-const navigationLinks = [
+const resourceLinks = [
   {
     href: "/resources",
     label: "Resources",
   },
+];
+
+const policyLinks = [
   {
     href: "/policies",
-    label: "Policies",
+    label: "Policy Search",
+  },
+  {
+    href: "/playground",
+    label: "Policy Playground",
+  },
+  {
+    href: "/policies/new",
+    label: "Create New Policy",
   },
 ];
+
+const navigationSections = [
+  {
+    title: "Resources",
+    links: resourceLinks,
+  },
+  {
+    title: "Policies",
+    links: policyLinks,
+  },
+];
+
+// const navigationLinks = [
+//   {
+//     href: "/resources",
+//     label: "Resources",
+//   },
+//   {
+//     href: "/policies",
+//     label: "Policies",
+//   },
+// ];
 
 const Header = () => {
   const { theme } = useTheme();
   const router = useRouter();
+  const [showNavigation, setShowNavigation] = useState(false);
+
+  const toggleNavigation = () => {
+    setShowNavigation(!showNavigation);
+  };
+
+  console.log("showNavigation", showNavigation);
+
+  // return (
+  //   <header className={`${styles.container} ${styles[theme]}`}>
+  //     <Link href={"/"}>
+  //       <a href={"/"}>
+  //         <RodeLogo theme={theme} />
+  //       </a>
+  //     </Link>
+  //     <div className={styles.links}>
+  //       {navigationLinks.map((link) => {
+  //         return (
+  //           <Link href={link.href} key={link.label}>
+  //             <a
+  //               className={
+  //                 router.pathname.startsWith(link.href)
+  //                   ? styles.active
+  //                   : styles.link
+  //               }
+  //             >
+  //               {link.label}
+  //             </a>
+  //           </Link>
+  //         );
+  //       })}
+  //     </div>
+  //     <ThemeToggle />
+  //   </header>
+  // );
 
   return (
     <header className={`${styles.container} ${styles[theme]}`}>
-      <Link href={"/"}>
-        <a href={"/"}>
-          <RodeLogo theme={theme} />
-        </a>
-      </Link>
-      <div className={styles.links}>
-        {navigationLinks.map((link) => {
+      <div className={styles.logoContainer}>
+        <Link href={"/"}>
+          <a href={"/"} className={styles.logo}>
+            <RodeLogo theme={theme} />
+          </a>
+        </Link>
+        <Button label={"Toggle Navigation"} buttonType={"icon"} onClick={toggleNavigation} className={showNavigation ? styles.expandedToggle : styles.hiddenToggle}>
+          <Icon
+            name={ICON_NAMES.MENU}
+            size={"xlarge"}
+          />
+        </Button>
+      </div>
+
+      <div className={showNavigation ? styles.expandedNavigation : styles.hiddenNavigation}>
+        {navigationSections.map((section) => {
           return (
-            <Link href={link.href} key={link.label}>
-              <a
-                className={
-                  router.pathname.startsWith(link.href)
-                    ? styles.active
-                    : styles.link
-                }
-              >
-                {link.label}
-              </a>
-            </Link>
+            <React.Fragment key={section.title}>
+              <p>{section.title}</p>
+              {section.links.map((link) => {
+                return (
+                  <Link href={link.href} key={link.label}>
+                    <a
+                      className={
+                        router.pathname.startsWith(link.href)
+                          ? styles.active
+                          : styles.link
+                      }
+                    >
+                      {link.label}
+                    </a>
+                  </Link>
+                );
+              })}
+            </React.Fragment>
           );
         })}
       </div>
-      <ThemeToggle />
     </header>
   );
 };
