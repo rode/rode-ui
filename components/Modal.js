dfe549878fad5e2fa697c71f7b4499b2c8a0018c
Jasmine Francois <jfrancois@leantechniques.com>
2021-04-20 09:22:41 -0500

    clean up comments

--- components/Modal.js
+++ components/Modal.js
@@ -26,7 +26,6 @@ const Modal = (props) => {
   const { title, children, onClose, isVisible } = props;
   const { theme } = useTheme();
 
-  // TODO: create a useSafeLayoutEffect hook to look for serverSide?
   useLayoutEffect(() => {
     if (isVisible) {
       document.getElementById("modalWrapper").scrollIntoView();
