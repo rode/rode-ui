d9a25ba3d6e7cbfc77e151560ca11f336ff41299
Jasmine Francois <jfrancois@leantechniques.com>
2021-05-06 09:14:19 -0500

    Tests around labelwithvalue component

--- components/LabelWithValue.js
+++ components/LabelWithValue.js
@@ -2,30 +2,25 @@ import React from "react";
 import PropTypes from "prop-types";
 import styles from "styles/modules/Typography.module.scss";
 
-// TODO: test this
-
 const LabelWithValue = (props) => {
-  const { label, value, className = "", as = "p" } = props;
-
-  const Text = as;
+  const { label, value, className = "" } = props;
 
   if (!value) {
     return null;
   }
 
   return (
-    <Text className={` ${styles.labelWithValueContainer} ${className}`}>
+    <p className={` ${styles.labelWithValueContainer} ${className}`}>
       <span className={styles.label}>{label}</span>
       <span className={styles.value}>{value}</span>
-    </Text>
+    </p>
   );
 };
 
 LabelWithValue.propTypes = {
   label: PropTypes.string.isRequired,
-  value: PropTypes.oneOfType([PropTypes.string, PropTypes.node]).isRequired,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
   className: PropTypes.string,
-  as: PropTypes.oneOf(["h1", "h2", "h3", "h4", "h5", "h6", "p", "span"]),
 };
 
 export default LabelWithValue;
