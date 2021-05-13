cd40a9741fe29f4efdced9dd85e92af98ae9c072
Jasmine Francois <jfrancois@leantechniques.com>
2021-05-13 11:48:07 -0500

    Add dark logo as favicon

--- pages/_app.js
+++ pages/_app.js
@@ -31,7 +31,23 @@ const MyApp = ({ Component, pageProps }) => {
           <React.Fragment>
             <Head>
               <title>Rode</title>
-              <link rel="icon" href="/favicon.ico" />
+              <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/apple-touch-icon.png"
+              />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/favicon-32x32.png"
+              />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/favicon-16x16.png"
+              />
               <link rel="preconnect" href="https://fonts.gstatic.com" />
               <link
                 href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,400&display=swap"
