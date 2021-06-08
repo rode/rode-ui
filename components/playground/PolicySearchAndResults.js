f8f811a2a392361d73f7735f4763c450ceb6d45b
Jasmine Francois <jfrancois@leantechniques.com>
2021-06-08 13:06:36 -0500

    Remove extra div from policy search

--- components/playground/PolicySearchAndResults.js
+++ components/playground/PolicySearchAndResults.js
@@ -77,49 +77,45 @@ const PolicySearchAndResults = ({ setPolicy, clearEvaluation }) => {
             }
           />
           {policySearch && (
-            <div className={styles.searchResultsContainer}>
-              <Loading loading={loading} type={"button"}>
-                {data?.length > 0 ? (
-                  <>
-                    {data.map((result) => (
-                      <div className={`${styles.searchCard}`} key={result.id}>
-                        <div>
-                          <p className={styles.cardHeader}>{result.name}</p>
-                          <p className={styles.cardText}>
-                            {result.description}
-                          </p>
-                        </div>
-                        <Button
-                          onClick={() => {
-                            setPolicy(result);
-                            setPolicySearch(false);
-                            dispatch({
-                              type: policyActions.SET_SEARCH_TERM,
-                              data: "",
-                            });
-                            setShowDrawer(false);
-                          }}
-                          buttonType={"text"}
-                          label={"Select Policy"}
-                          className={styles.actionButton}
-                        />
+            <Loading loading={loading} type={"button"}>
+              {data?.length > 0 ? (
+                <>
+                  {data.map((result) => (
+                    <div className={`${styles.searchCard}`} key={result.id}>
+                      <div>
+                        <p className={styles.cardHeader}>{result.name}</p>
+                        <p className={styles.cardText}>{result.description}</p>
                       </div>
-                    ))}
-                    {!isLastPage && (
                       <Button
-                        buttonType="text"
-                        onClick={goToNextPage}
-                        label={"See More Policies"}
-                        className={styles.viewMoreButton}
-                        id={"viewMorePoliciesButton"}
+                        onClick={() => {
+                          setPolicy(result);
+                          setPolicySearch(false);
+                          dispatch({
+                            type: policyActions.SET_SEARCH_TERM,
+                            data: "",
+                          });
+                          setShowDrawer(false);
+                        }}
+                        buttonType={"text"}
+                        label={"Select Policy"}
+                        className={styles.actionButton}
                       />
-                    )}
-                  </>
-                ) : (
-                  <p>{`No policies found matching "${state.searchTerm}"`}</p>
-                )}
-              </Loading>
-            </div>
+                    </div>
+                  ))}
+                  {!isLastPage && (
+                    <Button
+                      buttonType="text"
+                      onClick={goToNextPage}
+                      label={"See More Policies"}
+                      className={styles.viewMoreButton}
+                      id={"viewMorePoliciesButton"}
+                    />
+                  )}
+                </>
+              ) : (
+                <p>{`No policies found matching "${state.searchTerm}"`}</p>
+              )}
+            </Loading>
           )}
         </div>
       </Drawer>
