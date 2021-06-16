9bcb924ebad325d00eb136296da5cb518363d9b0
Jasmine Francois <jfrancois@leantechniques.com>
2021-06-16 11:49:47 -0500

    Create policy group form is working, no validation or follow-through

--- pages/policy-groups/new.js
+++ pages/policy-groups/new.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import React from "react";
+import React, {useState} from "react";
 import { useTheme } from "providers/theme";
 import { useRouter } from "next/router";
 import Button from "../../components/Button";
@@ -24,16 +24,60 @@ const PolicyGroups = () => {
   const { theme } = useTheme();
   const router = useRouter();
 
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = {
+      name,
+      description
+    };
+
+    const response = await fetch('/api/policy-groups', {
+      method: "POST",
+      body: JSON.stringify(formData),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    if (!response.ok) {
+      console.log('error occurred',response);
+      return;
+    }
+
+    const parsedResponse = await response.json();
+    console.log('here successful save', parsedResponse);
+
+  };
+
   return (
     <div>
       <p>New Policy Groups</p>
-      <form>
-
-      <Input name={'name'} label={'Policy Group Name'} onChange={() => {}}/>
-      <Input name={'description'} label={'Description'} onChange={() => {}}/>
-        <Button label={'Save Policy Group'} type={"submit"}/>
-        <Button label={'Cancel'} onClick={() => router.back()} buttonType={"text"}/>
-
+      <form onSubmit={onSubmit}>
+        <Input
+          name={"name"}
+          label={"Policy Group Name"}
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+          value={name}
+        />
+        <Input
+          name={"description"}
+          label={"Description"}
+          onChange={(event) => {
+            setDescription(event.target.value);
+          }}
+          value={description}
+        />
+        <Button label={"Save Policy Group"} type={"submit"} />
+        <Button
+          label={"Cancel"}
+          onClick={() => router.back()}
+          buttonType={"text"}
+        />
       </form>
     </div>
   );
