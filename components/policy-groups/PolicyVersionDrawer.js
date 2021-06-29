2c735e56d9e4575fe8fc058ee9cbaae91a217d13
Jasmine Francois <jfrancois@leantechniques.com>
2021-06-29 15:50:31 -0500

    Add styles to policy version drawer, show latest and selected versions to inform user

--- components/policy-groups/PolicyVersionDrawer.js
+++ components/policy-groups/PolicyVersionDrawer.js
@@ -17,20 +17,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Loading from "components/Loading";
-import styles from "styles/modules/PolicyGroupAssignments.module.scss";
+import styles from "styles/modules/PolicyVersionDrawer.module.scss";
 import { usePaginatedFetch } from "hooks/usePaginatedFetch";
 import Button from "components/Button";
 import Drawer from "components/Drawer";
 import { DATE_TIME_FORMAT } from "utils/constants";
-import dayjs from 'dayjs';
+import dayjs from "dayjs";
+import { useTheme } from "providers/theme";
+import LabelWithValue from "components/LabelWithValue";
+import Icon from "components/Icon";
+import { ICON_NAMES } from "utils/icon-utils";
 
-// TODO: show "selected" for current version
-// TODO: show "latest" on latest version
-
-const PolicyVersionDrawer = ({ isOpen, onClose, assignedPolicy, onVersionSelect }) => {
+const PolicyVersionDrawer = ({
+  isOpen,
+  onClose,
+  assignedPolicy,
+  onVersionSelect,
+}) => {
   if (!assignedPolicy) {
-    return null
+    return null;
   }
+
+  const { theme } = useTheme();
 
   const { data, loading, isLastPage, goToNextPage } = usePaginatedFetch(
     `/api/policies/${assignedPolicy.policyId}/versions`,
@@ -40,39 +48,62 @@ const PolicyVersionDrawer = ({ isOpen, onClose, assignedPolicy, onVersionSelect 
 
   return (
     <Drawer isOpen={isOpen} onClose={onClose}>
-      <div>
-        <p>{assignedPolicy.policyName}</p>
-        <p>Target a specific version of the policy when evaluating this policy group.</p>
+      <div className={`${styles[theme]}`}>
+        <div className={styles.drawerHeader}>
+          <p className={styles.policyName}>{assignedPolicy.policyName}</p>
+          <p className={styles.instructions}>Target a specific version of this policy</p>
+        </div>
+        <Loading loading={loading}>
+          {data ? (
+            <>
+              {data.map((version, index) => {
+                const isSelected = assignedPolicy.policyVersion === version.version;
+
+                return (
+                  <div key={version.id} className={styles.versionCard}>
+                    <div>
+                      <LabelWithValue
+                        label={"Version"}
+                        value={`${version.version} ${index === 0 ? " (latest)" : ""}`}
+                      />
+                      <LabelWithValue
+                        label={"Created"}
+                        value={dayjs(version.created).format(DATE_TIME_FORMAT)}
+                        className={styles.cardDetails}
+                      />
+                    </div>
+                    <div>
+                      <Button
+                        label={isSelected ? "Selected" : "Select Version"}
+                        buttonType={"text"}
+                        onClick={() => onVersionSelect(version, assignedPolicy)}
+                        disabled={isSelected}
+                      >
+                        {isSelected && (
+                          <>
+                            <Icon name={ICON_NAMES.CHECK} />
+                            <p>Selected</p>
+                          </>
+                        )}
+                      </Button>
+                    </div>
+                  </div>
+                );
+              })}
+            </>
+          ) : (
+            <p className={styles.noVersionsMessage}>No versions found</p>
+          )}
+          {!isLastPage && (
+            <Button
+              buttonType="text"
+              onClick={goToNextPage}
+              label={"View More"}
+              className={styles.viewMoreResultsButton}
+            />
+          )}
+        </Loading>
       </div>
-      <Loading loading={loading}>
-        {data ? (
-          <>
-            {data.map((version) => {
-              return (
-                <div key={version.id}>
-                  <div>
-                  <p>{version.version}</p>
-                    <p>{dayjs(version.created).format(DATE_TIME_FORMAT)}</p>
-                  </div>
-                  <div>
-                    <Button label={"Select Version"} onClick={() => onVersionSelect(version, assignedPolicy)}/>
-                  </div>
-                </div>
-              );
-            })}
-          </>
-        ) : (
-          <p>No versions found</p>
-        )}
-        {!isLastPage && (
-          <Button
-            buttonType="text"
-            onClick={goToNextPage}
-            label={"View More"}
-            className={styles.viewMoreResultsButton}
-          />
-        )}
-      </Loading>
     </Drawer>
   );
 };
@@ -81,7 +112,7 @@ PolicyVersionDrawer.propTypes = {
   isOpen: PropTypes.bool.isRequired,
   onClose: PropTypes.func.isRequired,
   assignedPolicy: PropTypes.object,
-  onVersionSelect: PropTypes.func.isRequired
+  onVersionSelect: PropTypes.func.isRequired,
 };
 
 export default PolicyVersionDrawer;
