4a5f8e3092c97ae4fffe47db24da288114524caf
Jasmine Francois <jfrancois@leantechniques.com>
2021-07-01 11:34:34 -0500

    Endpoint to fetch evaluations, component pulls and shows and ID of an evaluation

--- components/resources/ResourceEvaluationHistory.js
+++ components/resources/ResourceEvaluationHistory.js
@@ -19,16 +19,36 @@ import PropTypes from "prop-types";
 import styles from "styles/modules/ResourceEvaluationHistory.module.scss";
 import { useTheme } from "providers/theme";
 import { useAppState } from "providers/appState";
+import { usePaginatedFetch } from "../../hooks/usePaginatedFetch";
+import Loading from "../Loading";
 
 const ResourceEvaluationHistory = (props) => {
   const { resourceUri } = props;
   const { state } = useAppState();
   const { theme } = useTheme();
 
+  console.log('resourceUri', resourceUri);
+  const {data, loading, isLastPage, goToNextPage} = usePaginatedFetch(
+    resourceUri ? `/api/resources/${encodeURIComponent(resourceUri)}/resource-evaluations` : null, {}, 10
+  );
+
+  console.log('data', data);
   return (
     <div className={`${styles[theme]}`}>
      <p className={styles.header}>Evaluation History</p>
-
+      <Loading loading={loading}>
+        {data && data?.length ?
+          (
+            data.map((evaluation) => {
+              return (
+                <p>{evaluation.resourceEvaluation.id}</p>
+              )
+            })
+          )
+        :
+        <p>This resource has not been evaluated.</p>
+        }
+      </Loading>
     </div>
   );
 };
---
+++ pages/api/resources/[resourceUri]/resource-evaluations.js
@@ -0,0 +1,68 @@
+/**
+ * Copyright 2021 The Rode Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { StatusCodes, ReasonPhrases } from "http-status-codes";
+import { buildPaginationParams, get, getRodeUrl } from "pages/api/utils/api-utils";
+
+export default async (req, res) => {
+  if (req.method !== "GET") {
+    return res
+      .status(StatusCodes.METHOD_NOT_ALLOWED)
+      .json({ error: ReasonPhrases.METHOD_NOT_ALLOWED });
+  }
+
+  const rodeUrl = getRodeUrl();
+
+  try {
+    const { resourceUri } = req.query;
+
+    const params = {
+      ...buildPaginationParams(req),
+      resourceUri
+    };
+
+    if (req.query.filter) {
+      params.filter = req.query.filter;
+    }
+
+    const response = await get(
+      `${rodeUrl}/v1alpha1/resource-evaluations?${new URLSearchParams(params)}`
+    );
+
+    if (!response.ok) {
+      console.error(`Unsuccessful response from Rode: ${response.status}`);
+      return res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ error: ReasonPhrases.INTERNAL_SERVER_ERROR });
+    }
+
+    const listResourceEvaluationsResponse = await response.json();
+
+    // TODO: do some mapping here?
+    const evaluations = listResourceEvaluationsResponse.resourceEvaluations;
+
+    res.status(StatusCodes.OK).json({
+      data: evaluations,
+      pageToken: listResourceEvaluationsResponse.nextPageToken,
+    });
+  } catch (error) {
+    console.error("Error listing resource evaluations", error);
+
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: ReasonPhrases.INTERNAL_SERVER_ERROR });
+  }
+};
