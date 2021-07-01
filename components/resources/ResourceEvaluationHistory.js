e36abeff41ea5493bfbb8d21a5ad69119107cc3f
Jasmine Francois <jfrancois@leantechniques.com>
2021-07-01 11:22:35 -0500

    Add nav links to resource page, show empty evaluation history component

---
+++ components/resources/ResourceEvaluationHistory.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2021 The Rode Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "styles/modules/ResourceEvaluationHistory.module.scss";
+import { useTheme } from "providers/theme";
+import { useAppState } from "providers/appState";
+
+const ResourceEvaluationHistory = (props) => {
+  const { resourceUri } = props;
+  const { state } = useAppState();
+  const { theme } = useTheme();
+
+  return (
+    <div className={`${styles[theme]}`}>
+     <p className={styles.header}>Evaluation History</p>
+
+    </div>
+  );
+};
+
+ResourceEvaluationHistory.propTypes = {
+  resourceUri: PropTypes.string.isRequired,
+};
+
+export default ResourceEvaluationHistory;
